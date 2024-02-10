0e53c901cbcba89c88e4d263925d5bc41b0efb24
Sahitiakella <sahiti.akella@gmail.com>
2024-02-09 21:30:46 -0700

    Homepage layout done

---
+++ plotponder/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+      </Router>
+    </>
+  );
+}
+
+export default App;
